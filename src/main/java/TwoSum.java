ed894f8e6277a7fd1de3ca6a51a8e72ede3e884a
joanossoubita23 <joanossoubita@yahoo.fr>
2024-01-05 23:53:28 -0500

    f

--- src/main/java/TwoSum.java
+++ src/main/java/TwoSum.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class TwoSum {
-}
+import java.util.HashMap;
+
+public class TwoSum {
+        public int[] twoSum(int[] nums, int target) {
+            // Create a HashMap to store elements and their indices
+            HashMap<Integer, Integer> map = new HashMap<>();
+
+            for (int i = 0; i < nums.length; i++) {
+                int complement = target - nums[i];
+                // Check if the complement exists in the HashMap
+                if (map.containsKey(complement)) {
+                    // If found, return the indices of the two numbers
+                    return new int[]{map.get(complement), i};
+                }
+                // Otherwise, put the current number and its index into the HashMap
+                map.put(nums[i], i);
+            }
+
+            // If no solution is found, return an empty array or throw an exception
+            throw new IllegalArgumentException("No two sum solution");
+        }
+    }
+
+
